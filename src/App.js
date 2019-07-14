5a1a6e1004027ba21c04deaa1ce1647618875aba
Zixin Huang <huang.zixin@mail.utoronto.ca>
2019-07-14 06:24:40 -0400

    setup done

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { useEffect } from 'react';
+import 'materialize-css/dist/css/materialize.min.css';
+import M from 'materialize-css/dist/js/materialize.min.js';
+import './App.css';
+
+const App = () => {
+  useEffect(() => {
+    M.AutoInit();
+  });
+  return <div className="App">My App</div>;
+};
+
+export default App;
