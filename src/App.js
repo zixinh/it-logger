0fa63eda9c26017b8abb6422807b1a126720b8c9
Zixin Huang <huang.zixin@mail.utoronto.ca>
2019-07-17 13:28:50 -0400

    redux done for get logs and add log

--- src/App.js
+++ src/App.js
@@ -1,4 +1,14 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, Fragment } from 'react';
+import SearchBar from './components/layout/SearchBar';
+import Logs from './components/logs/Logs';
+import AddBtn from './components/layout/AddBtn';
+import AddLogModal from './components/logs/AddLogModal';
+import EditLogModal from './components/logs/EditLogModal';
+import AddTechModal from './components/techs/AddTechModal';
+import TechListModal from './components/techs/TechListModal';
+import { Provider } from 'react-redux';
+import store from './store';
+
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
 import './App.css';
@@ -7,7 +17,21 @@ const App = () => {
   useEffect(() => {
     M.AutoInit();
   });
-  return <div className="App">My App</div>;
+  return (
+    <Provider store={store}>
+      <Fragment>
+        <SearchBar />
+        <div className="container">
+          <AddBtn />
+          <Logs />
+          <AddLogModal />
+          <EditLogModal />
+          <AddTechModal />
+          <TechListModal />
+        </div>
+      </Fragment>
+    </Provider>
+  );
 };
 
 export default App;
---
+++ src/actions/logAction.js
@@ -0,0 +1,50 @@
+import { GET_LOGS, SET_LOADING, LOGS_ERROR, ADD_LOG } from './types';
+
+// Add a new log
+export const addLog = log => async dispatch => {
+  try {
+    setLoading();
+    const res = await fetch('/logs', {
+      method: 'POST',
+      body: JSON.stringify(log),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+    const data = await res.json();
+
+    dispatch({
+      type: ADD_LOG,
+      payload: data
+    });
+  } catch (error) {
+    dispatch({
+      type: LOGS_ERROR,
+      payload: error.response.data
+    });
+  }
+};
+
+export const getLogs = () => async dispatch => {
+  try {
+    setLoading();
+    const res = await fetch('/logs');
+    const data = await res.json();
+
+    dispatch({
+      type: GET_LOGS,
+      payload: data
+    });
+  } catch (error) {
+    dispatch({
+      type: LOGS_ERROR,
+      payload: error.response.data
+    });
+  }
+};
+
+export const setLoading = () => {
+  return {
+    type: SET_LOADING
+  };
+};
